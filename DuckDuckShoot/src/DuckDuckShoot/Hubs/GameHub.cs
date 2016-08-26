037ff458c3f720b5b84b33de2f37d6c341728630
Joseph Spearritt <joe.spearritt@live.com>
2016-08-26 21:10:54 +1000

    Add new vs stuff

---
+++ DuckDuckShoot/src/DuckDuckShoot/Hubs/GameHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.AspNetCore.SignalR.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DuckDuckShoot.Hubs
+{
+    [HubName("game")]
+    public class GameHub : Hub
+    {
+        public void Echo(string message)
+        {
+            Clients.Caller.echo(message);
+        }
+    }
+}
