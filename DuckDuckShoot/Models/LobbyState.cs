d533645ce3d7f35ed10a721e0e623ed432111d8b
CameronAavik <cameronaavik@gmail.com>
2016-08-27 14:31:49 +1000

    Added LobbyState class

---
+++ DuckDuckShoot/Models/LobbyState.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace DuckDuckShoot.Models
+{
+    public class LobbyState
+    {
+        [JsonProperty("names")]
+        public string[] Names { get; set; }
+        [JsonProperty("gameInProgress")]
+        public bool GameInProgress { get; set; }
+        [JsonProperty("aliveNames")]
+        public string[] AliveNames { get; set; }
+    }
+}
