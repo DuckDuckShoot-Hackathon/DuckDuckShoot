b0c20e1eaf7b8091fa44b44ea07a5b58606f74ee
CameronAavik <cameronaavik@gmail.com>
2016-08-27 10:35:54 +1000

    SignalR is finally working

---
+++ DuckDuckShoot/Hubs/GameHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace DuckDuckShoot.Hubs
+{
+    [HubName("game")]
+    public class GameHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.All.broadcastMessage(name, message);
+        }
+    }
+}
