7f07b254ba2b8493f0a9b1f850c1181c4c3a3aad
Joseph Spearritt <joe.spearritt@live.com>
2016-08-27 11:02:16 +1000

    Add skeleton code to game

--- DuckDuckShoot/Models/Game.cs
+++ DuckDuckShoot/Models/Game.cs
@@ -7,5 +7,43 @@ namespace DuckDuckShoot.Models
 {
     public class Game
     {
+        public enum Actions { DUCK, SHOOT };
+
+        public int InitialDucks { get; }
+
+        public List<Player> Players { get; }
+
+        public bool TurnCounting { get; set; }
+        public DateTime TurnEnd { get; set; }
+
+        public Game(List<User> users, int initialDucks)
+        {
+            InitialDucks = initialDucks;
+            Players = new List<Player>();
+
+            // Add all users in the lobby to the game
+            users.ForEach(user => Players.Add(new Player(user, InitialDucks)));
+
+            
+            TurnCounting = false;
+            TurnEnd = new DateTime();            
+        }
+
+        public void StartTurn()
+        {
+
+        }
+
+        public void AddPlayerAction()
+        {
+
+        }
+
+        public void ProcessTurn()
+        {
+
+        }
+
+        
     }
 }
--- DuckDuckShoot/Models/Player.cs
+++ DuckDuckShoot/Models/Player.cs
@@ -11,9 +11,9 @@ namespace DuckDuckShoot.Models
         // The User in the lobby that controls this player
         public User PlayerUser { get; }
 
-        // Whetehr this Player is alive
-
-        public User Alive { get; }
+        // Whether this Player is alive
+        private bool isAlive;
+        public bool IsAlive { get { return isAlive; } }
 
         // The number of ducks a player has left
         private int numDucks;
@@ -21,8 +21,28 @@ namespace DuckDuckShoot.Models
 
         public Player(User user, int numDucks)
         {
+            isAlive = true;
             PlayerUser = user;
             this.numDucks = numDucks;
+        }
+
+        /// <summary>
+        /// Set the player's status to dead
+        /// </summary>
+        public void Kill()
+        {
+            isAlive = false;
+        }
+
+        /// <summary>
+        /// Remove one duck from the player's count
+        /// </summary>
+        public void Duck()
+        {
+            if (NumDucks > 0)
+            {
+                numDucks--;
+            }
         }
 
     }
