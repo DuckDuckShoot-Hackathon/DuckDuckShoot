a475007dfae0845ad889c9ed6460a3b5de0b7203
CameronAavik <cameronaavik@gmail.com>
2016-08-27 11:42:34 +1000

    Changed from Home to Game

---
+++ DuckDuckShoot/Controllers/GameController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace DuckDuckShoot.Controllers
+{
+    public class GameController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
