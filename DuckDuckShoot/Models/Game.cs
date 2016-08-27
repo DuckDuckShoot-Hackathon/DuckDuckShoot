d1e8a92d6071a223a30e016d8e341e237955963a
Joseph Spearritt <joe.spearritt@live.com>
2016-08-27 11:40:20 +1000

    Add turn processing logic

--- DuckDuckShoot/Models/Game.cs
+++ DuckDuckShoot/Models/Game.cs
@@ -38,12 +38,58 @@ namespace DuckDuckShoot.Models
 
         public void AddPlayerAction(Player player, Action action)
         {
-            TurnActions.Add(player, action);
+            // Only alive players can add actions
+            if (player.IsAlive)
+            {
+                TurnActions.Add(player, action);
+            }
         }
 
         public void ProcessTurn(object state)
         {
+            var actions = GetActionList();
+            
+            // Figure out who is ducking
+            var duckingPlayers = new HashSet<Player>();
+            foreach (Action action in actions)
+            {
+                // Duck only if they have Ducks left
+                if (action.Actor.IsAlive && action.Actor.NumDucks > 0
+                    && action.ActType == Action.ActionType.DUCK)
+                {
+                    duckingPlayers.Add(action.Actor);
+                    action.Actor.Duck();
+                }
+            }
 
+            // Perform shoot actions
+            foreach (Action action in actions)
+            {
+                if (action.ActType == Action.ActionType.SHOOT)
+                {
+                    // Only kill the player if they are alive and not ducking
+                    if (action.Target.IsAlive && !duckingPlayers.Contains(action.Target))
+                    {
+                        action.Target.Kill();
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Get the actions performed this turn, in order
+        /// </summary>
+        /// <returns></returns>
+        public List<Action> GetActionList()
+        {
+            List<Action> result = new List<Action>();
+
+            foreach (KeyValuePair<Player, Action> entry in TurnActions)
+            {
+                result.Add(entry.Value);
+            }
+
+            return result;
         }
 
         public class Action
@@ -51,10 +97,12 @@ namespace DuckDuckShoot.Models
             public enum ActionType { SHOOT, DUCK };
 
             public ActionType ActType { get; }
+            public Player Actor { get; }
             public Player Target { get; }
 
-            public Action(ActionType type, Player target)
+            public Action(ActionType type, Player actor, Player target)
             {
+                this.Actor = actor;
                 this.ActType = type;
                 this.Target = target;
             }
