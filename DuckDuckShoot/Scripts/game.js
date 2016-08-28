26c8504578d6aeacc806a92992a6fa935aeb6fad
Astrid Farmer <astridfarmer1@gmail.com>
2016-08-28 15:37:19 +1000

    remove black dot

--- DuckDuckShoot/Scripts/game.js
+++ DuckDuckShoot/Scripts/game.js
@@ -348,7 +348,8 @@ function deleteAllUsers() {
     distributePlayers();
 }
 
-function deleteUser(username) {
+function deleteUser(player) {
+    var username = player["PlayerUser"]["Name"];
     $('#' + username)
     .attr('deleting', 'true')
     .css({
